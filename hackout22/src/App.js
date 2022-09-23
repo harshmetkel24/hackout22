2cc584065d216d00cc5790bdcd315be5f5c99942
unknown <harshmetkel24@gmail.com>
2022-09-24 03:48:51 +0530

    tutorial layout created

--- hackout22/src/App.js
+++ hackout22/src/App.js
@@ -1,10 +1,17 @@
 import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} /> 
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- hackout22/src/pages/Home.js
+++ hackout22/src/pages/Home.js
@@ -1,11 +1,33 @@
 import React from "react";
+import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div>hello, welcome</div>
+      <div className="container">
+        <div
+          className="text-center display-5 font-weight-bold"
+          style={{ fontWeight: 500, color: "  #f37335" }}
+        >
+          What services we provide?
+        </div>
+        <div className="tutorial-container container-fluid d-flex justify-content-around flex-wrap">
+          <Card
+            icon={<i class="fa-solid fa-route"></i>}
+            step={"Select Route"}
+          />
+          <Card
+            icon={<i class="fa-solid fa-clock"></i>}
+            step={"Select Waiting Time"}
+          />
+          <Card
+            icon={<i class="fa-solid fa-square-poll-vertical"></i>}
+            step={"Our Prediction"}
+          />
+        </div>
+      </div>
     </div>
   );
 };
